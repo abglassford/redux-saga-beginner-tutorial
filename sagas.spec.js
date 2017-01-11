965591e27c1e424ff6181a0db9b067466c13ee0e
Alex Glassford <a.b.glassford@gmail.com>
2017-01-11 13:57:43 -0700

    complete tests

--- sagas.spec.js
+++ sagas.spec.js
@@ -1,13 +1,29 @@
 import test from 'tape';
 
-import { incrementAsync } from './sagas'
+import { incrementAsync } from './sagas';
+import { delay } from 'redux-saga';
+import { put, call } from 'redux-saga/effects';
 
 test('incrementAsync Saga test', (assert) => {
-  const gen = incrementAsync()
+  const gen = incrementAsync();
+
+  assert.deepEqual(
+    gen.next().value,
+      call(delay, 1000),
+    'incrementAsync should return a Promise that will resolve after 1 second'
+  )
+
+  assert.deepEqual(
+    gen.next().value,
+    put({type: 'INCREMENT'}),
+    'incrementAsync Saga must dispatch an INCREMENT action'
+  )
 
   assert.deepEqual(
     gen.next(),
-    {done: false, value: ??? },
-    'incrementAsync should return a Promise that will resolve after 1 second'
+    {done: true, value: undefined },
+    'incrementAsync Saga must be done'
   )
+
+  assert.end();
 });
